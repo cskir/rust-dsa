1dccb25a464a30dfd1e2885fa40080a3d6486f90
csaba kiraly <cskiraly27@gmail.com>
2025-11-09 20:29:58 +0100

    two_sum

---
+++ src/easy/two_sum.rs
@@ -0,0 +1,40 @@
+use std::collections::HashMap;
+pub struct Solution {}
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map = HashMap::new();
+
+        for (i, item) in nums.iter().enumerate() {
+            let complement = target - item;
+            if let Some(index) = map.get(&complement) {
+                return vec![i as i32, *index as i32];
+            }
+            map.insert(item, i);
+        }
+
+        vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        let nums = vec![2, 7, 11, 15];
+        let target = 9;
+        let result = Solution::two_sum(nums, target);
+        assert_eq!(result, vec![0, 1]);
+
+        let nums = vec![3, 2, 4];
+        let target = 6;
+        let result = Solution::two_sum(nums, target);
+        assert_eq!(result, vec![1, 2]);
+
+        let nums = vec![3, 3];
+        let target = 6;
+        let result = Solution::two_sum(nums, target);
+        assert_eq!(result, vec![0, 1]);
+    }
+}
